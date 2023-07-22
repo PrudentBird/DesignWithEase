a40cc614a6c1aecd120463210cb70afdf353757e
PrudentBird <prudentbird@gmail.com>
2023-07-22 16:04:20 +0100

    Version History

--- Sources/script.js
+++ Sources/script.js
@@ -1,6 +1,33 @@
 const menuBtn = document.querySelector(".menu-icon"),
-    openMenuBtn = document.querySelector(".menu-icon");
+      navBar = document.querySelector(".navbar"),
+      navLinks = document.querySelectorAll(".nav-link"),
+      overlay = document.querySelector(".overlay"),
+      darkModeToggle = document.querySelector(".dark-mode"),
+      htmlController = document.querySelector("html");
 
-menuBtn.addEventListener("click", () => {
-    openMenuBtn.classList.toggle("openmenu");
+const toggleMenu = () => {
+    menuBtn.classList.toggle("openmenu");
+    htmlController.classList.toggle("responsive");
+    navBar.classList.toggle("openmenu");
+};
+
+menuBtn.addEventListener("click", toggleMenu);
+overlay.addEventListener("click", toggleMenu);
+navLinks.forEach((navLink) => {
+    navLink.addEventListener("click", toggleMenu);
 });
+
+darkModeToggle.addEventListener("click", () => {
+    htmlController.classList.toggle("dark");
+});
+
+
+function handleScroll() {
+    if (window.scrollY > 0) {
+        navBar.classList.add('navbar-shadow');
+    } else {
+        navBar.classList.remove('navbar-shadow');
+    }
+}
+
+window.addEventListener('scroll', handleScroll);
